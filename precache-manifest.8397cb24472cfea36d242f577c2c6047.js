c120946fe406b782586f499abd86d19da5188041
kuldeep <kuldeepdandeliya@gmail.com>
2021-05-24 16:19:51 +0530

    Updates

---
+++ precache-manifest.8397cb24472cfea36d242f577c2c6047.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f33ba59cb953ef62f027856dc21e23a",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "c5bab95eece533e7a3cf",
+    "url": "/reactApp/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "715c37fb720d883c07d0",
+    "url": "/reactApp/static/css/main.aa6a75b0.chunk.css"
+  },
+  {
+    "revision": "c5bab95eece533e7a3cf",
+    "url": "/reactApp/static/js/2.c22b75ae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactApp/static/js/2.c22b75ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715c37fb720d883c07d0",
+    "url": "/reactApp/static/js/main.5010afe7.chunk.js"
+  },
+  {
+    "revision": "23cb2e8d5a4f45e36fbd",
+    "url": "/reactApp/static/js/runtime-main.48a8f0f5.js"
+  }
+]);
